a13648976346d6c2419474ff9633762948cd85c7
Gabriel Felipe Kuhn Schiestl <gabriel.schiestl25@hotmail.com>
2026-03-22 17:54:07 -0300

    feat: add binary protocol parser

--- parser/parser.c
+++ parser/parser.c
@@ -74,78 +74,50 @@ void process_command(char *cmd, char *key, char *value, uint64_t ttl, int fd) {
         }
 
         send_error("could not expire entry", fd, 1);
-    } else if(strcmp(cmd, "REMAINING") == 0) {
-        char *resp = process_remaining(key);
-        send_success(resp, fd, 1);
-        free(resp);
-    } else {
-        send_error("command not allowed", fd, 1);
-    }
+    } 
+    // else if(strcmp(cmd, "REMAINING") == 0) {
+    //     char *resp = process_remaining(key);
+    //     send_success(resp, fd, 1);
+    //     free(resp);
+    // } else {
+    //     send_error("command not allowed", fd, 1);
+    // }
 }
 
 void handle_client_message(char* message, int client_fd) {
-    printf("Message received: %s", message);
     int index = 0;
 
-    char cmd_len_char = message[index];
-    int cmd_len;
+    int cmd_len = (unsigned char) message[index++];
+    char *cmd = strndup(message + index, cmd_len);
+    index += cmd_len;
 
-    sscanf(&cmd_len_char, "%d", &cmd_len);
+    int key_len = (unsigned char) message[index++];
+    char *key = strndup(message + index, key_len);
+    index += key_len;
 
-    char *cmd = strndup(message+1, cmd_len);
-    index += cmd_len+1;
+    int val_exists = (unsigned char) message[index++];
 
-    char key_len_char = message[index];
-    int key_len;
+    char *val = NULL;
 
-    sscanf(&key_len_char, "%d", &key_len);
+    if (val_exists == 1) {
+        uint16_t val_len;
+        memcpy(&val_len, message + index, sizeof(uint16_t));
+        val_len = ntohs(val_len);
+        index += sizeof(uint16_t);
 
-    char *key = strndup(message+index+1, key_len);
-    index += key_len+1;
-
-    char val_exists_char = message[index];
-    int val_exists;
-
-    sscanf(&val_exists_char, "%d", &val_exists);
-
-    index++;
-    char *val;
-
-    if(val_exists == 1) {
-        char val_len_char = message[index];
-        uint16_t val_len_big_end;
-
-        int val_len_int = sscanf(&val_len_char, "%" SCNu16, &val_len_big_end);
-        uint16_t val_len = ntohs(val_len_big_end);
-
-        val = strndup(message+index+1, val_len);
-        index += val_len+1;
+        val = strndup(message + index, val_len);
+        index += val_len;
     }
-    printf("Value: %s", val);
-    printf("%d\n", index);
-    char ttl_exists_char = message[index];
-    int ttl_exists;
 
-    sscanf(&ttl_exists_char, "%d", &ttl_exists);
-    printf("TTL EXISTS %d, %c\n", ttl_exists, ttl_exists_char);
-    index++;
+    int ttl_exists = (unsigned char) message[index++];
+
     uint32_t ttl = 0;
 
-    if(ttl_exists == 1) {
+    if (ttl_exists == 1) {
         uint32_t ttl_big_end;
-
-        sscanf(message+index, "%" SCNu32, &ttl_big_end);
-        ttl = ntohs(ttl_big_end);
-    }
-    printf("AAAAAA %u\n", ttl);
-    if(!cmd) {
-        send_error("command not received", client_fd, 1);
-        return;
-    }
-
-    if(!key) {
-        send_error("key not received", client_fd, 1);
-        return;
+        memcpy(&ttl_big_end, message + index, sizeof(uint32_t));
+        ttl = ntohl(ttl_big_end);
+        index += sizeof(uint32_t);
     }
 
     printf("%s, %s, %s, %u\n",
@@ -156,6 +128,10 @@ void handle_client_message(char* message, int client_fd) {
     );
 
     process_command(cmd, key, val, ttl, client_fd);
+
+    free(cmd);
+    free(key);
+    if (val) free(val);
 }
 
 
--- processor/remaining.c
+++ processor/remaining.c
@@ -1,25 +1,25 @@
-#include "../store/store.h"
+// #include "../store/store.h"
 
-char* process_remaining(char* key) {
-    Entry *result = table_get(key);
-    if(!result) {
-        return NULL;
-    }
+// char* process_remaining(char* key) {
+//     Entry *result = table_get(key);
+//     if(!result) {
+//         return NULL;
+//     }
 
-    long remaining = 0;
-    if(result->has_ttl) {
-        if(result->expire_at <= time(NULL)) {
-            remaining = -1;
-        } else {
-            remaining = time(NULL) - result->expire_at;
-        }
-    } 
+//     long remaining = 0;
+//     if(result->has_ttl) {
+//         if(result->expire_at <= time(NULL)) {
+//             remaining = -1;
+//         } else {
+//             remaining = time(NULL) - result->expire_at;
+//         }
+//     } 
 
-    int n = snprintf(NULL, 0, "%s %ld", "SUCCESS", remaining);
+//     int n = snprintf(NULL, 0, "%s %ld", "SUCCESS", remaining);
 
-    char *response = malloc(n + 1);
+//     char *response = malloc(n + 1);
 
-    snprintf(response, n + 1, "%s %ld", "SUCCESS", remaining);
+//     snprintf(response, n + 1, "%s %ld", "SUCCESS", remaining);
 
-    return response;
-}
+//     return response;
+// }
